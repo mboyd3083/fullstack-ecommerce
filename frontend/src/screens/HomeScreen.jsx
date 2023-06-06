1cfbd4fab7197c222013eca1170d464392515aea
Mike Boyd <mboyd3083@gmail.com>
2023-06-06 13:44:05 -0400

    updated homescreen to fetch products from backend

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,9 +1,25 @@
+import {useEffect,useState} from "react";
 import { Row, Col } from "react-bootstrap";
-import products from "../products";
 import Product from "../components/Product";
-import Rating from "../components/Rating";
+import axios from "axios";
 
 const HomeScreen = () => {
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const { data } = await axios.get("/api/products");
+        setProducts(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    fetchProducts();
+  }, []);
+
   return (
     <>
       <h1>Latest Products</h1>
@@ -14,7 +30,6 @@ const HomeScreen = () => {
           </Col>
         ))}
       </Row>
-      {/* <Rating value = {3} /> */}
     </>
   );
 };
