81c4102a75e719ac27cc620a9bebd5e787490302
Mike Boyd <mboyd3083@gmail.com>
2023-06-06 13:43:18 -0400

    set up ratings component

--- frontend/src/components/Rating.jsx
+++ frontend/src/components/Rating.jsx
@@ -1,25 +1,54 @@
 import { FaStar, FaStarHalfAlt, FaRegStar } from "react-icons/fa";
-import ReviewStars from "./ReviewStars";
+
 const Rating = ({ value, text }) => {
-    const totalNumOfStars = 5; 
-
-    const review = (num) =>{
-
-        for(let i = 0; i < num; i++){
-        return( <ReviewStars value={value} starIndex={i + 1} />);
-        }
-    }
-
   return (
     <div className="rating">
-    {review(5)}
-    
-     {/* <ReviewStars value = {value} starIndex={1}/>
-     <ReviewStars value = {value} starIndex={2}/>
-     <ReviewStars value = {value} starIndex={3}/>
-     <ReviewStars value = {value} starIndex={4}/>
-     <ReviewStars value = {value} starIndex={5}/> */}
-
+      <span>
+        {value >= 1 ? (
+          <FaStar />
+        ) : value <= 0.5 ? (
+          <FaStarHalfAlt />
+        ) : (
+          <FaRegStar />
+        )}
+      </span>
+      <span>
+        {value >= 2 ? (
+          <FaStar />
+        ) : value <= 1.5 ? (
+          <FaStarHalfAlt />
+        ) : (
+          <FaRegStar />
+        )}
+      </span>
+      <span>
+        {value >= 3 ? (
+          <FaStar />
+        ) : value <= 2.5 ? (
+          <FaStarHalfAlt />
+        ) : (
+          <FaRegStar />
+        )}
+      </span>
+      <span>
+        {value >= 4 ? (
+          <FaStar />
+        ) : value <= 3.5 ? (
+          <FaStarHalfAlt />
+        ) : (
+          <FaRegStar />
+        )}
+      </span>
+      <span>
+        {value >= 5 ? (
+          <FaStar />
+        ) : value <= 4.5 ? (
+          <FaStarHalfAlt />
+        ) : (
+          <FaRegStar />
+        )}
+      </span>
+      <span className="rating-text">{text && text}</span>
     </div>
   );
 };
