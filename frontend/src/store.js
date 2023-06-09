6d8750117cdb33f661f62efa3105c574220f33a7
Mike Boyd <mboyd3083@gmail.com>
2023-06-09 12:46:20 -0400

    refactored

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./slices/apiSlice";
-import cartSliceReducer from './slices/cartSlice'
+import cartSliceReducer from "./slices/cartSlice";
 
 const store = configureStore({
   reducer: {
@@ -13,5 +13,3 @@ const store = configureStore({
 });
 
 export default store;
-
-
