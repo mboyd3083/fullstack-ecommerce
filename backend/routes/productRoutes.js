5131fd60b53d5a3adf989ffb864e890e37b775b1
Mike Boyd <mboyd3083@gmail.com>
2023-07-12 11:47:59 -0400

    added new product routes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -8,16 +8,22 @@ import {
   deleteProduct,
   createProductReview,
   getTopProducts,
+  createCategory,
+  getCategories,
+  getCategoryById,
+  getLatestProducts,
 } from "../controllers/productController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
 router.route("/").get(getProducts).post(protect, admin, createProduct);
+router.route("/category").get(getCategories).post(protect, admin, createCategory);
 router.get("/top", getTopProducts);
+router.get("/latest", getLatestProducts);
 router
   .route("/:id")
   .get(getProductById)
   .put(protect, admin, updateProduct)
   .delete(protect, admin, deleteProduct);
 router.route("/:id/reviews").post(protect, createProductReview);
-
+router.route("/category/:id").get(getCategoryById);
 export default router;
