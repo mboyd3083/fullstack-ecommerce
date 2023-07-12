64a3a43dcd7c7de04b079c93286c6aee0d44fb9e
Mike Boyd <mboyd3083@gmail.com>
2023-07-12 11:42:49 -0400

    Added images

---
+++ frontend/src/assets/images/brand/index.js
@@ -0,0 +1,7 @@
+import appleLogo from "./apple-logo.png";
+import psLogo from "./playstation-logo.png";
+import xboxLogo from "./xbox-logo.png";
+import hpLogo from "./hp-logo.png";
+import cannonLogo from "./cannon-logo.jpeg";
+import logitechLogo from "./logitech-logo.png";
+export { appleLogo, psLogo,xboxLogo,hpLogo,cannonLogo,logitechLogo};
---
+++ frontend/src/assets/images/category/index.js
@@ -0,0 +1,14 @@
+import audioImage from './beats-image.jpeg';
+import computerImage from './macbook3.png';
+import phoneImage from './iphone2.jpg';
+import videoGameImage from './console.jpg';
+import cameraImage from './camera.jpeg';
+
+
+export {
+    audioImage,
+    computerImage,
+    phoneImage,
+    videoGameImage,
+    cameraImage
+}
