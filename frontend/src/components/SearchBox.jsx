71a808554b711dded0f53e6a26ac19ffb5d5bcce
Mike Boyd <mboyd3083@gmail.com>
2023-07-12 11:52:13 -0400

    updated

--- frontend/src/components/SearchBox.jsx
+++ frontend/src/components/SearchBox.jsx
@@ -13,7 +13,7 @@ const SearchBox = () => {
     if (keyword) {
       setKeyword("");
       navigate(`/search/${keyword.trim()}`);
-      setKeyword('')
+      setKeyword("");
     } else {
       navigate(`/`);
     }
@@ -27,8 +27,7 @@ const SearchBox = () => {
         value={keyword}
         placeholder="Search Products..."
         className="mr-sm-2 ml-sm-5"
-      >
-      </Form.Control>
+      ></Form.Control>
       <Button type="submit" variant="outline-light" className="p-2 mx-2">
         Search
       </Button>
