a4487f599c72abd8164b767c22b77e47295d9df1
Mike Boyd <mboyd3083@gmail.com>
2023-07-17 12:29:40 -0400

    created custom banners

---
+++ frontend/src/components/Banner.jsx
@@ -0,0 +1,8 @@
+const Banner = () => {
+  return (
+    <div className = "banner_container">
+    <h1 className = "banner_text">FREE 1-3 DAY SHIPPING OVER $100</h1>
+    </div>
+  )
+}
+export default Banner
---
+++ frontend/src/components/SummerDealBanner.jsx
@@ -0,0 +1,10 @@
+const SummerDealBanner = () => {
+  return (
+    <div className="summer_deal_banner_container">
+      <h1 className="summer_deal_banner_text">
+        Get Up To 50% Off Select Items
+      </h1>
+    </div>
+  );
+};
+export default SummerDealBanner;
